507e58cb887edb71d2aad47efd38bba28a59fa37
Timothy Luong <Timothy.Luong@ou.edu>
2021-03-21 01:47:00 -0500

    Finished the code

--- CollectionAggregateFunctions/AggregateFunction/Program.cs
+++ CollectionAggregateFunctions/AggregateFunction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AggregateFunction
 {
@@ -6,7 +7,58 @@ namespace AggregateFunction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaring Variables
+            List<double> grades = new List<double>();
+            string answer;
+            double grade,sum = 0, index = 0, modeValue = 0, count;
+
+            //Asking the user for exam grades 
+            Console.WriteLine("Please enter in your exam grade:");
+            answer = Console.ReadLine();
+            do
+            {
+                while (double.TryParse(answer,out grade)==false)
+                {
+                    Console.WriteLine("You did not enter in a grade. Please try again!");
+                    answer = Console.ReadLine();
+                }
+                grades.Add(grade);
+                Console.WriteLine("Please enter in your next exam grade! Type \'q\' to quit.");
+                answer = Console.ReadLine();
+            } while (answer != "q");
+            
+            //Sortting the exam grade
+            grades.Sort();
+
+            //Finding the total for the exam grades
+            for (int i = 0; i < grades.Count; i++)
+            {
+                sum += grades[i];
+            }
+
+            //Calculating the mode
+            for (int i = 0; i < grades.Count; i++)
+            {
+                count = 0;
+                for (int j = 0; j < grades.Count; j++)
+                {
+                    if (grades[j] == grades[i])
+                    {
+                        ++count;
+                    }
+                }
+                if (count > index)
+                {
+                    index = count;
+                    modeValue = grades[i];
+                }
+            } 
+           
+            //Outputting the results to the user
+            Console.WriteLine($"The min grade is {grades[0]}");
+            Console.WriteLine($"The max grade is {grades[grades.Count-1]}");
+            Console.WriteLine($"The avg grade is {(sum / grades.Count).ToString("N2")}");
+            Console.WriteLine($"The mode grade is {modeValue}");
         }
     }
 }
