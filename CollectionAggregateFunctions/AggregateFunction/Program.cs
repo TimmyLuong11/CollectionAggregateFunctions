9babc264970371e4d810b5f9ecae02e4879a941d
Timothy Luong <Timothy.Luong@ou.edu>
2021-03-23 17:58:06 -0500

    Added a new mode method.

--- CollectionAggregateFunctions/AggregateFunction/Program.cs
+++ CollectionAggregateFunctions/AggregateFunction/Program.cs
@@ -9,8 +9,10 @@ namespace AggregateFunction
         {
             //Declaring Variables
             List<double> grades = new List<double>();
+            Dictionary<double, int> examGrade = new Dictionary<double, int>();
             string answer;
-            double grade,sum = 0, index = 0, modeValue = 0, count;
+            double grade,sum = 0;
+            int maxOccurrences = 0;
 
             //Asking the user for exam grades 
             Console.WriteLine("Please enter in your exam grade:");
@@ -23,12 +25,18 @@ namespace AggregateFunction
                     answer = Console.ReadLine();
                 }
                 grades.Add(grade);
+                if (examGrade.ContainsKey(grade) == false)
+                {
+                    examGrade.Add(grade, 1);
+                }
+                else
+                {
+                    examGrade[grade] = examGrade[grade] + 1;
+                }
                 Console.WriteLine("Please enter in your next exam grade! Type \'q\' to quit.");
                 answer = Console.ReadLine();
-            } while (answer != "q");
+            } while (answer.ToLower() != "q");
             
-            //Sortting the exam grade
-            grades.Sort();
 
             //Finding the total for the exam grades
             for (int i = 0; i < grades.Count; i++)
@@ -37,28 +45,23 @@ namespace AggregateFunction
             }
 
             //Calculating the mode
-            for (int i = 0; i < grades.Count; i++)
+            foreach (double key in examGrade.Keys)
             {
-                count = 0;
-                for (int j = 0; j < grades.Count; j++)
+                double exam = key;
+                if (examGrade[grade] > maxOccurrences)
                 {
-                    if (grades[j] == grades[i])
-                    {
-                        ++count;
-                    }
+                    maxOccurrences = examGrade[grade];
                 }
-                if (count > index)
-                {
-                    index = count;
-                    modeValue = grades[i];
-                }
-            } 
-           
+            }
+
+            //Sortting the exam grade
+            grades.Sort();
+
             //Outputting the results to the user
-            Console.WriteLine($"The min grade is {grades[0]}");
-            Console.WriteLine($"The max grade is {grades[grades.Count-1]}");
-            Console.WriteLine($"The avg grade is {(sum / grades.Count).ToString("N2")}");
-            Console.WriteLine($"The mode grade is {modeValue}");
+            Console.WriteLine($"The minimum grade is {grades[0]}.");
+            Console.WriteLine($"The maximum grade is {grades[grades.Count-1]}.");
+            Console.WriteLine($"The average grade is {(sum / grades.Count).ToString("N2")}.");
+            Console.WriteLine($"The grade that appears the most time is {grade} with {maxOccurrences} occurrence.");
         }
     }
 }
